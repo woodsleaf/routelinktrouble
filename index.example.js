37c831c6f3a0a11726e0f24f66b320ec52bd86db
woodsleaf <freepostpair@gmail.com>
2019-09-05 03:50:04 +0300

    nottested add vue support

---
+++ index.example.js
@@ -0,0 +1,32 @@
+////console.log('Привет, Мир!')
+// Импортировать модуль с использованием синтаксиса CommonJS
+////const dep = require('./path/to/dep')
+// Импорт модуля с использованием синтаксиса импорта ES6
+////import dep from './path/to/dep'
+// Импортировать файл CSS
+////import './test.css'
+// Импортировать файл CSS с помощью CSS-модулей
+////import classNames from './test.css'
+// Импортировать URL-адрес в файл изображения
+////import imageURL from './test.png'
+
+//URL должен быть статически анализируемым
+////import fs from 'fs'
+// Чтение содержимого в виде строки
+////const string = fs.readFileSync(__dirname + '/test.txt', 'utf8')
+// Чтение содержимого в качестве Buffer
+////const buffer = fs.readFileSync(__dirname + '/test.png')
+
+/* Импортировать другой файл CSS */
+////@import './other.css';
+/* //Ссылка на файл изображения
+    .test {
+    background: url('./images/background.png');
+    }
+*/
+////import './custom.scss'
+
+/* //https://ru.parceljs.org/code_splitting.html
+    import 'babel-polyfill'
+    import './app'
+*/
